fc338b7f10552593ac22eb924b43a7ab02ddd0bf
Turyahabwa Mark <profturyahabwa@gmail.com>
2022-10-29 13:50:06 +0300

    init: App setup

---
+++ flaskr/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",  # should be overridden with a random value when deploying.
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
+    )
+
+    if test_config:
+        app.config.from_mapping(test_config) # will be used instead of the instance configuration. so the tests can be configured independently of any development values
+    else:
+        app.config.from_pyfile("config.py", silent=True) #  when deploying, this can be used to set a real SECRET_KEY.
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "Hello World"
+
+    return app
