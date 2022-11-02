2a5b71b80960f6a302a81f892ebc36d6aefe3207
Turyahabwa Mark <profturyahabwa@gmail.com>
2022-11-02 12:50:23 +0300

    cd: add staging config

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,16 +5,17 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)    
     app.config.from_mapping(
-        SECRET_KEY="dev",  # should be overridden with a random value when deploying.
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
     )
 
     if test_config:
         app.config.from_mapping(test_config) # will be used instead of the instance configuration. so the tests can be configured independently of any development values
     else:
-        app.config.from_pyfile("config.py", silent=True) #  when deploying, this can be used to set a real SECRET_KEY.
+        env_config = os.getenv("DEPLOY_SETTINGS", "config.DevelopmentConfig")
+        app.config.from_object(env_config)
+        # app.config.from_pyfile("config.py", silent=True) #  when deploying, use the above 2 lines to set real SECRETS and comment this line
 
     # ensure the instance folder exists
     try:
